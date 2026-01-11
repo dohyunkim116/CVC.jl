6bbcdda72d3c0993f8cd55a82020265992414193
Do Hyun Kim <dkim116@ucla.edu>
2026-01-11 12:08:21 -0800

    Ensure minimum SNPs for jackknife blocks is 2 and add assertion for component SNP count

--- src/CVC.jl
+++ src/CVC.jl
@@ -284,9 +284,10 @@ function cvc(
     
     # Dynamically adjust J if any component has fewer SNPs than J
     min_M_k = minimum(M_k)
+    @assert min_M_k >= 2 "Each component must have at least 2 SNPs"
     if min_M_k <= J
         J_old = J
-        J = max(10, floor(Int, min_M_k/2))  # Ensure J doesn't go below 10
+        J = max(2, min_M_k)  # Ensure J doesn't go below 2
         @info "Reducing jackknife blocks from $J_old to $J because smallest component has only $min_M_k SNPs"
     end
     
